49c05c24fffc555c7e072578da635f55404f2a9b
Andrew Hamilton <andrew.hamilton@gmail.com>
2017-07-05 15:44:24 -0400

    making sure reject rules are always current

--- macaddr.c
+++ macaddr.c
@@ -4,8 +4,46 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+char rule[256];
+
+void updateRejectRule(char *macAddr){
+    /* delete old reject rule, if any */
+    sprintf(rule, "iptables -D FORWARD -m mac --mac-source %s -j REJECT", macAddr);
+    printf("%s\n", rule);
+    printf("result: %d\n", system(rule));
+    /* insert a new reject rule at the top */
+    sprintf(rule, "iptables -I FORWARD -m mac --mac-source %s -j REJECT", macAddr);
+    printf("%s\n", rule);
+    printf("result: %d\n", system(rule));
+}
+
+int insertAcceptRule(char *macAddr, char *dateTime){
+    /* insert new accept rule above all other rules */
+    if (dateTime) {
+        sprintf(rule, "iptables -I FORWARD -m mac --mac-source %s -j ACCEPT -m time --datestop %s", macAddr, dateTime);
+    } else {
+        sprintf(rule, "iptables -I FORWARD -m mac --mac-source %s -j ACCEPT", macAddr);
+    }
+    printf("%s\n", rule);
+    int rc = system(rule);
+    printf("result: %d\n", rc);
+    return rc;
+}
+
+int deleteAcceptRule(char *macAddr, char *dateTime){
+    /* remove accept rule */
+    if (dateTime) {
+        sprintf(rule, "iptables -D FORWARD -m mac --mac-source %s -j ACCEPT -m time --datestop %s", macAddr, dateTime);
+    } else {
+        sprintf(rule, "iptables -D FORWARD -m mac --mac-source %s -j ACCEPT", macAddr);
+    }
+    printf("%s\n", rule);
+    int rc = system(rule);
+    printf("result: %d\n", rc);
+    return rc;
+}
+
 int main(int argc, char *argv[]){
-    char rule[256];
     int rc = 0;
 
 	if (argc >= 2) {
@@ -13,24 +51,12 @@ int main(int argc, char *argv[]){
 			printf("usage: %s (on|off) mac_address [expiry_time]\n", argv[0]);
 		} else if (argc >= 3) {
 			setuid(0);
+			updateRejectRule(argv[2]);
 			if (strcmp(argv[1], "on") == 0) {
-				if (argc >= 4) {
-					sprintf(rule, "iptables -I FORWARD -m mac --mac-source %s -j ACCEPT -m time --datestop %s", argv[2], argv[3]);
-				} else {
-					sprintf(rule, "iptables -I FORWARD -m mac --mac-source %s -j ACCEPT", argv[2]);
-				}
-				printf("%s\n", rule);
-				rc = system(rule);
-				printf("result: %d\n", rc);
+			    deleteAcceptRule(argv[2], argc >=4 ? argv[3] : NULL); /* delete old rule, just in case */
+			    rc = insertAcceptRule(argv[2], argc >=4 ? argv[3] : NULL);
 			} else if (strcmp(argv[1], "off") == 0) {
-				if (argc >= 4) {
-					sprintf(rule, "iptables -D FORWARD -m mac --mac-source %s -j ACCEPT -m time --datestop %s", argv[2], argv[3]);
-				} else {
-					sprintf(rule, "iptables -D FORWARD -m mac --mac-source %s -j ACCEPT", argv[2]);
-				}
-				printf("%s\n", rule);
-				rc = system(rule);
-				printf("result: %d\n", rc);
+			    rc = deleteAcceptRule(argv[2], argc >=4 ? argv[3] : NULL);
 			}
 		}
 	} else {
